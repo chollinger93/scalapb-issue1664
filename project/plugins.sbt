1659c475cc03d7ea611dd059e88029537bb9190f
Christian Hollinger <christian@chollinger.com>
2024-04-04 10:13:50 -0400

    testing fix via public_constructor_parameters

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,3 @@
 addSbtPlugin("com.thesamet" % "sbt-protoc" % "1.0.0")
 
-libraryDependencies += "com.thesamet.scalapb" %% "compilerplugin" % "0.11.15+39-bb6489f7-SNAPSHOT"
+libraryDependencies += "com.thesamet.scalapb" %% "compilerplugin" % "0.11.15+40-9a6f8c79-SNAPSHOT" // "0.11.15"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,15 +4,17 @@ import test.nested.NestedExampleEvent
 import test.nested.NestedExampleEvent.given
 import com.sksamuel.avro4s.Encoder as AvroEncoder
 import com.sksamuel.avro4s.AvroSchema
+import org.apache.avro.Schema
 
 trait Thing[A <: scalapb.GeneratedMessage : AvroEncoder]
 
 case object ExampleThing extends Thing[NestedExampleEvent]:
-  val enc = AvroEncoder[NestedExampleEvent]
-  val schema = AvroSchema[NestedExampleEvent]
+  val enc: AvroEncoder[NestedExampleEvent] = AvroEncoder[NestedExampleEvent]
+  val schema: Schema = AvroSchema[NestedExampleEvent]
 
 @main def hello() = {
   println(f"Hello, ${ExampleThing.schema}")
   assert(ExampleThing.schema.toString(false)
     .equals("{\"type\":\"record\",\"name\":\"NestedExampleEvent\",\"namespace\":\"test.nested\",\"fields\":[{\"name\":\"id\",\"type\":\"string\",\"default\":\"\"},{\"name\":\"action\",\"type\":\"string\",\"default\":\"Undefined\"}]}"))
+
 }
